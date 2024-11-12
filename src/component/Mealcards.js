7ece1f17aa526601401f92d5e21ce44e5e7ee110
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-12 20:19:21 +0300

    updates src components

--- src/component/Mealcards.js
+++ src/component/Mealcards.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 
 const Mealcards = ({detail}) => {
+    //const handleSavedRec
     console.log(detail)
     return (
         <div>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,15 +1,26 @@
-// Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import '../App.css';
+import './Navbar.css';
 
 const Navbar = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('dark-theme', !darkMode);
+    };
+
     return (
-        <nav className="navbar">
-            <ul>
-                <a><NavLink to="/">Home</NavLink></a>
-                <a><NavLink to="/mainpage">Recipes</NavLink></a>
+        <nav className={`navbar ${darkMode ? 'dark' : ''}`}>
+            <h1 className="logo">PRecipe🍲📜🥄</h1>
+            <ul className="nav-links">
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/mainpage">Recipes</NavLink></li>
+                <li><NavLink to="/saved-recipes">Saved Recipes</NavLink></li>
             </ul>
+            <button className="theme-toggle" onClick={toggleDarkMode}>
+                {darkMode ? '🌞 Light' : '🌙 Dark'}
+            </button>
         </nav>
     );
 };
